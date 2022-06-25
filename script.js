590d9cbaa11054635db4b63aab48f6719531814b
Lorena <lorebe2508@gmail.com>
2022-06-25 01:25:27 -0300

    Encriptador Mejorado

---
+++ script.js
@@ -0,0 +1,35 @@
+
+ 
+function encriptar() {
+    var texto = document.getElementById("InputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm, "enter");
+    var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+    var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+    var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+    var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
+
+    document.getElementById("imgright").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display ="inherit";
+}
+ 
+function desencriptar() {
+ var texto = document.getElementById("InputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm, "e");
+    var txtCifrado = txtCifrado.replace(/ober/igm, "o");
+    var txtCifrado = txtCifrado.replace(/imes/igm, "i");
+    var txtCifrado = txtCifrado.replace(/ai/igm, "a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
+
+    document.getElementById("imgright").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display ="inherit";
+}
+function copiarTexto() {
+    var texto = document.getElementById("texto2").value;
+    document.getElementById("InputTexto").value=texto;
+}
